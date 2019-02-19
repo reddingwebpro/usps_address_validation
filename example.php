d5c95596a09e3f25abb71fca3a61884a69ccd755
Jason Olson <46981729+reddingwebpro@users.noreply.github.com>
2019-02-19 13:50:55 -0800

    Create example.php

---
+++ example.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: reddingWebPro
+ * Date: 2/18/2019
+ * Version 1.0
+ * Time: 7:05 PM
+ */
+
+// example code shown below:
+
+require ('USPS.php');
+use RedWebDev\USPS;
+$uspsZip = new USPS('YOUR_API_KEY_HERE');  // insert your api key from USPS
+
+$address = '1600 Amphitheatre Parkway'; // address line is required
+$city  = 'Mountain View';
+$state = 'CA';  //looking for the two character state
+
+$return = $uspsZip->getZipCode($address,$city,$state);
+echo "<ul>";
+echo "<li>Address: ".$return['Address1'];
+echo "<li>City: ".$return['City];
+echo "<li>State: ".$return['State'];
+echo "<li>Zip: ".$return['Zip5'];
